3b1c0dbdc44caeebf61059a9a3b0ed21598afc84
onebyteone <onebyte@one>
2021-02-16 19:45:02 -0500

    add token distributor

---
+++ contracts/token/TokenDistributor.sol
@@ -0,0 +1,101 @@
+pragma solidity 0.6.12;
+
+import "../common/IERC20.sol";
+import "../common/SafeERC20.sol";
+import "../common/SafeMath.sol";
+
+interface DAOFunding {
+    function notifyRewardAmount(uint _amount) external;
+}
+
+// A token distributor that distribute tokens to addresses according to specific distribution rules
+contract TokenDistributor {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
+    address public token;
+
+    address public governance;
+
+    address public team;
+    uint public teamAllocation = 4000;
+
+    address public dao;
+    uint public daoAllocation = 4000;
+
+    uint public constant max = 10000;
+
+    // Remainder goes into treasury
+    address public treasury;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor (
+        address _token,
+        address _team,
+        address _dao,
+        address _treasury
+    ) public {
+        token = _token;
+        team = _team;
+        dao = _dao;
+        treasury = _treasury;
+        governance = msg.sender;
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function distribute() public {
+        uint _balance = IERC20(token).balanceOf(address(this));
+
+        uint _teamAmount = _balance.mul(teamAllocation).div(max);
+        IERC20(token).safeTransfer(team, _teamAmount);
+
+        uint _daoAmount = _balance.mul(daoAllocation).div(max);
+        IERC20(token).safeTransfer(dao, _daoAmount);
+        DAOFunding(dao).notifyRewardAmount(_daoAmount);
+
+        uint _treasuryAmount = _balance.sub(_teamAmount).sub(_daoAmount);
+        IERC20(token).safeTransfer(treasury, _treasuryAmount);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function setGovernance(address _governance) {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function setTeam(address _team) {
+        require(msg.sender == governance, "!governance");
+        team = _team;
+    }
+
+    function setDao(address _dao) {
+        require(msg.sender == governance, "!governance");
+        dao = _dao;
+    }
+
+    function setTreasury(address _treasury) {
+        require(msg.sender == governance, "!governance");
+        treasury = _treasury;
+    }
+
+    function setTeamAllocation(address _teamAllocation) {
+        require(msg.sender == governance, "!governance");
+        teamAllocation = _teamAllocation;
+    }
+
+    function setDaoAllocation(address _daoAllocation) {
+        require(msg.sender == governance, "!governance");
+        daoAllocation = _daoAllocation;
+    }
+
+    function rescue(address _token) {
+        require(msg.sender == governance, "!governance");
+        uint _balance = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).safeTransfer(governance, _balance);
+    }
+}
