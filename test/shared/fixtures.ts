0730f91f11a9809780ffbb957eb236ffb3fb050f
E <>
2021-05-24 00:08:25 -0400

    update test

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -11,9 +11,9 @@ import TokenDistributor from '../../build/contracts/TokenDistributor.json'
 import RewardDistributor from '../../build/contracts/RewardDistributor.json'
 import Controller from '../../build/contracts/Controller.json'
 import WrappedERC20 from '../../build/contracts/WrappedERC20.json'
-import Vault from '../../build/contracts/Vault.json'
+import VaultCompoundUSDT from '../../build/contracts/BaseVault.json'
 import TokenMaster from '../../build/contracts/TokenMaster.json'
-import StrategyUSDTCompound from '../../build/contracts/StrategyUSDTCompound.json'
+import StrategyCompoundUSDT from '../../build/contracts/BaseStrategy.json'
 import MultiStakingRewards from '../../build/contracts/MultiStakingRewards.json'
 
 
@@ -47,17 +47,18 @@ export async function getFixture(provider: Web3Provider, [wallet]: Wallet[]): Pr
     const ald = await deployContract(wallet, ALDToken)
     const treasury = await deployContract(wallet, Treasury)
     const dao = await deployContract(wallet, DAO, [usdt.address, expandTo18Decimals(1), 20000, [wallet.address]])
-    const tokenDistributor = await deployContract(wallet, TokenDistributor,  [[wallet.address]])
+    const tokenDistributor = await deployContract(wallet, TokenDistributor)
     const rewardDistributor = await deployContract(wallet, RewardDistributor,  [[wallet.address]])
     const controller = await deployContract(wallet, Controller)
     const wrappedAld = await deployContract(wallet, WrappedERC20, [ald.address,  "Wrapped Aladdin DAO Token", "wALD"])
-    const tokenMaster = await deployContract(wallet, TokenMaster, [ald.address,  tokenDistributor.address])
+    let block = await provider.getBlock('latest')
+    const tokenMaster = await deployContract(wallet, TokenMaster, [ald.address,  tokenDistributor.address, block.number])
 
-    const vault = await deployContract(wallet, Vault, [usdt.address,  comp.address, controller.address, tokenMaster.address], overrides)
-    const strategyUSDTCompound = await deployContract(wallet, StrategyUSDTCompound, [controller.address],overrides )
+    const vault = await deployContract(wallet, VaultCompoundUSDT, [usdt.address,  comp.address, controller.address, tokenMaster.address], overrides)
+    const strategyUSDTCompound = await deployContract(wallet, StrategyCompoundUSDT, [controller.address, usdt.address, comp.address],overrides )
     const multiStakingRewards = await deployContract(wallet, MultiStakingRewards, [ald.address, wrappedAld.address, rewardDistributor.address], overrides)
 
- 
+
     return {
         ald,
         treasury,
